cd89500d5259c02ae42d7dc0e947e8a41a1b180a
grazomarin <huseynovk355@gmail.com>
2022-10-07 18:36:43 +0400

    refactored project addition
    -projects are now stored in a UI myProjects array

--- src/index.js
+++ src/index.js
@@ -6,13 +6,13 @@ const nav = document.querySelector('.container-nav')
 
 const UI = (() => {
 
-    const ProjectList = []
+    const myProjects = []
 
     function initProject() {
         const index = ProjectFactory('Index')
         const indexTaskForm = TaskFormFactory(index)
         UI.resetMain()
-        ProjectList.push(index)
+        myProjects.push(index)
         UI.displayProjectList()
         indexTaskForm.buildAddTask()
 
@@ -25,40 +25,25 @@ const UI = (() => {
         }
     }
 
-    const buildNav = () => {
-        nav.innerHTML = `
-        <li class="container-nav_addProject">add new project</li>
-        <form action="" class="container-nav-projectForm">
-        <input class="container-nav-projectForm_title" type="text" placeholder="Title">
-        <div class="container-nav-projectForm-buttons">
-        <button type="reset" class="container-nav-projectForm-buttons_cancel">Cancel</button>
-        <button type="reset" class="container-nav-projectForm-buttons_submit">Submit</button>
-        </div>
-        </form>
-        `
+    const addProject = (project) => {
+        myProjects.push(project)
     }
 
     const displayProjectList = () => {
-        ProjectList.forEach(project => {
+        myProjects.forEach(project => {
             project.displayProject()
         })
     }
 
-    const addTaskEventListener = (project) => {
-        const addTaskButton = document.querySelector('.container-main_addTask')
-        addTaskButton.addEventListener('click', () => {
-
-        })
-    }
-
-
     return {
+        myProjects,
         resetMain,
-        buildNav,
         initProject,
+        addProject,
         displayProjectList,
     }
 })()
+
 
 const ProjectFactory = (titleValue) => {
     const myTasks = []
@@ -90,6 +75,7 @@ const ProjectFactory = (titleValue) => {
         projectCont.append(projectName, projectTaskCount)
         nav.prepend(projectCont)
     }
+
     return {
         title,
         myTasks,
@@ -98,83 +84,7 @@ const ProjectFactory = (titleValue) => {
         displayProjectTasks,
         displayProject,
     }
-
-
 }
-
-const TaskFormFactory = (parentProject) => {
-
-    const addTaskLine = document.createElement('div')
-    const taskForm = document.createElement('form')
-    const inputs = document.createElement('div')
-    const title = document.createElement('input')
-    const body = document.createElement('textarea')
-    const buttons = document.createElement('div')
-    const cancel = document.createElement('button')
-    const submit = document.createElement('button')
-
-    const buildAddTask = () => {
-        addTaskLine.classList = 'container-main_addTask'
-        taskForm.classList = 'container-main-taskForm'
-        inputs.classList = 'container-main-taskForm-inputs'
-        title.classList = 'container-main-taskForm-inputs_title'
-        body.classList = 'container-main-taskForm-inputs_description'
-        buttons.classList = 'container-main-taskForm-buttons'
-        cancel.classList = 'container-main-taskForm-buttons_cancel'
-        submit.classList = 'container-main-taskForm-buttons_submit'
-
-        addTaskLine.textContent = 'add task'
-        cancel.textContent = 'cancel'
-        submit.textContent = 'submit'
-
-        title.setAttribute('placeholder', 'Title')
-        body.setAttribute('rows', '3')
-        body.setAttribute('multiline', 'false')
-        body.setAttribute('placeholder', 'Description')
-        cancel.setAttribute('type', 'reset')
-        submit.setAttribute('type', 'reset')
-
-        buttons.append(cancel, submit)
-        inputs.append(title, body)
-        taskForm.append(inputs, buttons)
-        main.append(addTaskLine, taskForm)
-        initAddTaskEventListeners()
-    }
-
-    const initAddTaskEventListeners = () => {
-        addTaskLine.addEventListener('click', () => { showAddTask() })
-        cancel.addEventListener('click', () => { hideAddTask() })
-        submit.addEventListener('click', () => { submitTask() })
-    }
-
-    const submitTask = () => {
-        const newTask = TaskFactory(title.value, body.value, false)
-        parentProject.myTasks.push(newTask)
-        newTask.displayTask()
-        hideAddTask()
-    }
-
-    const showAddTask = () => {
-        addTaskLine.style.display = 'none'
-        taskForm.style.display = 'block'
-    }
-
-    const hideAddTask = () => {
-        addTaskLine.style.display = 'block'
-        taskForm.style.display = 'none'
-    }
-
-
-    return {
-        buildAddTask,
-        hideAddTask,
-        showAddTask,
-        submitTask,
-        initAddTaskEventListeners,
-    }
-}
-
-
 
 
 const TaskFactory = (titleVal, bodyVal, completionVal) => {
@@ -240,11 +150,148 @@ const TaskFactory = (titleVal, bodyVal, completionVal) => {
     }
 }
 
+
+const TaskFormFactory = (parentProject) => {
+
+    const addTaskLine = document.createElement('div')
+    const taskForm = document.createElement('form')
+    const inputs = document.createElement('div')
+    const title = document.createElement('input')
+    const body = document.createElement('textarea')
+    const buttons = document.createElement('div')
+    const cancel = document.createElement('button')
+    const submit = document.createElement('button')
+
+    const buildAddTask = () => {
+        addTaskLine.classList = 'container-main_addTask'
+        taskForm.classList = 'container-main-taskForm'
+        inputs.classList = 'container-main-taskForm-inputs'
+        title.classList = 'container-main-taskForm-inputs_title'
+        body.classList = 'container-main-taskForm-inputs_description'
+        buttons.classList = 'container-main-taskForm-buttons'
+        cancel.classList = 'container-main-taskForm-buttons_cancel'
+        submit.classList = 'container-main-taskForm-buttons_submit'
+
+        addTaskLine.textContent = 'add task'
+        cancel.textContent = 'cancel'
+        submit.textContent = 'submit'
+
+        title.setAttribute('placeholder', 'Title')
+        body.setAttribute('rows', '3')
+        body.setAttribute('multiline', 'false')
+        body.setAttribute('placeholder', 'Description')
+        cancel.setAttribute('type', 'reset')
+        submit.setAttribute('type', 'reset')
+
+        buttons.append(cancel, submit)
+        inputs.append(title, body)
+        taskForm.append(inputs, buttons)
+        main.append(addTaskLine, taskForm)
+        initEventListeners()
+    }
+
+    const initEventListeners = () => {
+        addTaskLine.addEventListener('click', () => { showAddTask() })
+        cancel.addEventListener('click', () => { hideAddTask() })
+        submit.addEventListener('click', () => { submitTask() })
+    }
+
+    const submitTask = () => {
+        const newTask = TaskFactory(title.value, body.value, false)
+        parentProject.myTasks.push(newTask)
+        newTask.displayTask()
+        hideAddTask()
+    }
+
+    const showAddTask = () => {
+        addTaskLine.style.display = 'none'
+        taskForm.style.display = 'block'
+    }
+
+    const hideAddTask = () => {
+        addTaskLine.style.display = 'block'
+        taskForm.style.display = 'none'
+    }
+
+    console.log(UI.myProjects);
+
+
+    return {
+        buildAddTask,
+        hideAddTask,
+        showAddTask,
+        submitTask,
+        initEventListeners,
+    }
+}
+
+
+const navFormFactory = () => {
+    const addProjectLine = document.createElement('div')
+    const projectForm = document.createElement('form')
+    const title = document.createElement('input')
+    const buttons = document.createElement('div')
+    const cancel = document.createElement('button')
+    const submit = document.createElement('button')
+
+    const buildNav = () => {
+        addProjectLine.classList = 'container-nav_addProject'
+        projectForm.classList = 'container-nav-projectForm'
+        title.classList = 'container-nav-projectForm_title'
+        buttons.classList = 'container-nav-projectForm-buttons'
+        cancel.classList = 'container-nav-projectForm-buttons_cancel'
+        submit.classList = 'container-nav-projectForm-buttons_submit'
+
+        addProjectLine.textContent = 'add project'
+        cancel.textContent = 'cancel'
+        submit.textContent = 'submit'
+
+        title.setAttribute('placeholder', 'Title')
+        cancel.setAttribute('type', 'reset')
+        submit.setAttribute('type', 'reset')
+
+        buttons.append(cancel, submit)
+        projectForm.append(title, buttons)
+        nav.append(addProjectLine, projectForm)
+        initEventListeners()
+    }
+
+    const initEventListeners = () => {
+        addProjectLine.addEventListener('click', () => { showAddTask() })
+        cancel.addEventListener('click', () => { hideAddTask() })
+        submit.addEventListener('click', () => { submitTask() })
+    }
+
+    const submitTask = () => {
+        const newProject = ProjectFactory(title.value)
+        UI.addProject(newProject)
+        newProject.displayProject()
+        hideAddTask()
+    }
+
+    const showAddTask = () => {
+        addProjectLine.style.display = 'none'
+        projectForm.style.display = 'flex'
+    }
+
+    const hideAddTask = () => {
+        addProjectLine.style.display = 'block'
+        projectForm.style.display = 'none'
+    }
+
+
+    return {
+        buildNav,
+    }
+
+}
+
 //reset main
-UI.buildNav()
+const navForm = navFormFactory()
+navForm.buildNav()
 UI.initProject()
 
 
 
 
-// make add project work
+// make add project work`
