5f9f7f142cf67b9b579a02c4b4c356365b4d1ae3
grazomarin <huseynovk355@gmail.com>
2023-03-07 15:02:49 +0400

    feat: switch to webfont instead of locally loaded font

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Index from "./components/Index";
-import Main from "./components/Main";
+import React, { useState, useEffect } from 'react';
+import WebFont from 'webfontloader';
+import Header from './components/Header';
+import Index from './components/Index';
+import Main from './components/Main';
 
 function App() {
+	useEffect(() => {
+		WebFont.load({
+			google: {
+				families: ['Jost'],
+			},
+		});
+	}, []);
+
   return (
     <>
       <Header />
