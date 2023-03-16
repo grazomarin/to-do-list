d64000ee36fb0ad90258e885ceb83f6ad82bde3f
grazomarin <huseynovk355@gmail.com>
2023-03-16 15:17:04 +0400

    feat(taskform): add taskform saves inputed values in a state, add styling

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,17 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 function TaskForm({ toggleDisplay, handleSubmit }) {
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+
+	const inputRef = useRef(null);
+
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
+
 	return (
 		<form>
-			<input type="text" name="title" />
-			<textarea
-				name="details"
-				id="details"
-				cols="30"
-				rows="10"
-			></textarea>
+			<div className="input-cont">
+				<input
+					type="text"
+					name="title"
+					ref={inputRef}
+					onInput={(e) => setTitle(e.target.value)}
+					placeholder="Title"
+				/>
+				<textarea
+					name="description"
+					id="description"
+					rows="5"
+					onInput={(e) => setDescription(e.target.value)}
+					placeholder="Details..."
+				></textarea>
+			</div>
 			<div className="buttons">
-				<button className="submit" type="submit" onClick={handleSubmit}>
+				<button
+					className="submit"
+					type="submit"
+					onClick={(e) => {
+						handleSubmit(e, {
+							title: title,
+							description: description,
+						});
+					}}
+				>
 					Submit
 				</button>
 				<button className="cancel" type="reset" onClick={toggleDisplay}>
